2aedb4cc8fc816a276209ffc02cb4bbc36fe9f21
Commander <commander@echofive.net>
2025-10-20 21:56:06 +0000

    +groups

--- main.py
+++ main.py
@@ -10,6 +10,10 @@ def main():
 
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
     player = Player(SCREEN_WIDTH / 2, SCREEN_HEIGHT / 2)
+
+    updateable = pygame.sprite.Group()
+    drawable = pygame.sprite.Group()
+    Player.containers = (updateable, drawable)
     
     clock = pygame.time.Clock()
     dt = 0
@@ -19,8 +23,8 @@ def main():
                 return
 
         screen.fill("black", rect=None, special_flags=0)
-        player.draw(screen)
-        player.update(dt)
+        drawable.draw(screen)
+        updateable.update(dt)
         pygame.display.flip()
         dt = (clock.tick(60) / 1000)
 
