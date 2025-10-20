7a8e323fbe10464ff5d9781733b507aea5288652
Commander <commander@echofive.net>
2025-10-20 19:54:57 +0000

    +quit

--- main.py
+++ main.py
@@ -10,7 +10,12 @@ def main():
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
     
     while True:
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                return
+
         screen.fill("black", rect=None, special_flags=0)
+        display.flip()
 
 if __name__ == "__main__":
     main()
