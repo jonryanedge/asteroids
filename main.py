32e7c7285f665ca0abf8134d95a93a030ba9bebe
Commander <commander@echofive.net>
2025-10-20 21:35:10 +0000

    +player

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 import pygame
 from constants import *
+from player import *
 
 def main():
     pygame.init()
@@ -8,6 +9,7 @@ def main():
     print(f"Screen height: {SCREEN_HEIGHT}")
 
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+    player = Player(SCREEN_WIDTH / 2, SCREEN_HEIGHT / 2)
     
     clock = pygame.time.Clock()
     dt = 0
@@ -19,6 +21,8 @@ def main():
         screen.fill("black", rect=None, special_flags=0)
         pygame.display.flip()
         dt = (clock.tick(60) / 1000)
+        player.draw(screen)
+
 
 if __name__ == "__main__":
     main()
