83ab9f34ee03fb4b2ed7b64b3b5a4505b0bde1b8
Commander <commander@echofive.net>
2025-10-20 23:48:42 +0000

    wip2

--- player.py
+++ player.py
@@ -27,7 +27,7 @@ class Player(CircleShape):
         self.position += forward * PLAYER_MOVE_SPEED * dt
 
     def shoot(self, dt):
-        fire = Shot(self.x, self.y)
+        fire = Shot(self.position, self.position)
         fire.velocity = pygame.Vector2(0, 1).rotate(self.rotation)
         fire.position += fire.velocity * PLAYER_SHOOT_SPEED * dt
 
